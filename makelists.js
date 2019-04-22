e32b927496430586f31bf8a551a92123148e49b1
blatank <paddocknote@gmail.com>
2019-04-22 14:41:26 +0000

    jsファイル内を少し実装。やりたいことが少しだけ見えるようにした。

---
+++ makelists.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+    // element
+    const text = document.getElementById('text');
+    const ulBtn = document.getElementById('ulBtn');
+    const olBtn = document.getElementById('olBtn');
+
+    ulBtn.onclick = (event) => {
+        // テキストを取得する
+        // 各行を<li>タグで囲む
+        // 全体を<ul>タグで囲む
+        // クリップボードにコピーする
+    }
+
+    olBtn.onclick = (event) => {
+        // テキストを取得する
+        // 各行を<li>タグで囲む
+        // 全体を<ol>タグで囲む
+        // クリップボードにコピーする
+    }
+
+})();
